f2b151ee6c16a4c775772ed323c869c4356a94c6
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-09-04 16:10:20 +0300

    Implement controller methods in library-service
    - added model mapper dependency and configuration
    - created new endpoints

--- library-service/src/main/java/com/artsem/api/libraryservice/controller/BookStatusController.java
+++ library-service/src/main/java/com/artsem/api/libraryservice/controller/BookStatusController.java
@@ -1,11 +1,45 @@
 package com.artsem.api.libraryservice.controller;
 
+import com.artsem.api.libraryservice.model.BooksStatus;
+import com.artsem.api.libraryservice.model.dto.EngagedBookStatusDto;
+import com.artsem.api.libraryservice.model.dto.FreeBookStatusDto;
+import com.artsem.api.libraryservice.service.BookStatusService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/status")
+@RequestMapping("/api/v1/library")
 public class BookStatusController {
+
+    private final BookStatusService bookStatusService;
+
+    private final ModelMapper mapper;
+
+    @GetMapping("/books/free")
+    public ResponseEntity<List<FreeBookStatusDto>> getAllFreeBooks() {
+        List<BooksStatus> freeBooks = bookStatusService.getAllFreeBooks();
+        List<FreeBookStatusDto> freeBooksDTO = freeBooks.stream()
+                .map(book -> mapper.map(book, FreeBookStatusDto.class)).toList();
+        return ResponseEntity.ok(freeBooksDTO);
+    }
+
+    @PutMapping("/books/{id}/take")
+    public ResponseEntity<EngagedBookStatusDto> takeBook(@PathVariable Long id) {
+        BooksStatus booksStatus = bookStatusService.setStatusAsEngaged(id);
+        EngagedBookStatusDto bookStatusDTO = mapper.map(booksStatus, EngagedBookStatusDto.class);
+        return ResponseEntity.ok(bookStatusDTO);
+    }
+
+    @PutMapping("/books/{id}/return")
+    public ResponseEntity<FreeBookStatusDto> returnBook(@PathVariable Long id) {
+        BooksStatus booksStatus = bookStatusService.setStatusAsFree(id);
+        FreeBookStatusDto bookStatusDTO = mapper.map(booksStatus, FreeBookStatusDto.class);
+        return ResponseEntity.ok(bookStatusDTO);
+    }
+
 }
