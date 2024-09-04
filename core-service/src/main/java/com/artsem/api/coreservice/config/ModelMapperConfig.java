0314acb8b68b0efb74ad6318e7f749509fb807b0
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-09-04 12:56:21 +0300

    Modify core-service
    - added modelMapper dependency
    - created modelMapper bean

---
+++ core-service/src/main/java/com/artsem/api/coreservice/config/ModelMapperConfig.java
@@ -0,0 +1,13 @@
+package com.artsem.api.coreservice.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
