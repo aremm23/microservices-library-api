9e6d48cb6a8c5c216e1972eb59984d090e1c69da
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-09-04 12:28:18 +0300

    Modify core-service
    - add basic crud operations to BookService and implement them

--- core-service/src/main/java/com/artsem/api/coreservice/service/BookService.java
+++ core-service/src/main/java/com/artsem/api/coreservice/service/BookService.java
@@ -1,4 +1,14 @@
 package com.artsem.api.coreservice.service;
 
+import com.artsem.api.coreservice.model.Book;
+
+import java.util.List;
+
 public interface BookService {
+    List<Book> getAllBooks();
+    Book getBookById(Long id);
+    Book getBookByIsbn(String id);
+    Book addBook(Book book);
+    Book updateBook(Long id, Book updatedBook);
+    void deleteBookById(Long id);
 }
--- core-service/src/main/java/com/artsem/api/coreservice/service/impl/BookServiceImpl.java
+++ core-service/src/main/java/com/artsem/api/coreservice/service/impl/BookServiceImpl.java
@@ -1,7 +1,66 @@
 package com.artsem.api.coreservice.service.impl;
 
+import com.artsem.api.coreservice.model.Book;
+import com.artsem.api.coreservice.repository.BookRepository;
 import com.artsem.api.coreservice.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
 
+    private final BookRepository repository;
+
+    @Override
+    public List<Book> getAllBooks() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Book getBookById(Long id) {
+        return repository.findById(id).orElseThrow(
+                () -> new NoSuchElementException("Book with id %d not found".formatted(id))
+        );
+    }
+
+    @Override
+    public Book getBookByIsbn(String isbn) {
+        return repository.findByIsbn(isbn).orElseThrow(
+                () -> new NoSuchElementException("Book with isbn %s not found".formatted(isbn))
+        );
+    }
+
+    @Override
+    public Book addBook(Book book) {
+        return repository.save(book);
+    }
+
+    @Override
+    public Book updateBook(Long id, Book updatedBook) {
+        Book existingBook = repository.findById(id).orElseThrow(
+                () -> new NoSuchElementException("Book with id %s not found".formatted(id))
+        );
+        parseUpdatedToExisted(updatedBook, existingBook);
+        return repository.save(existingBook);
+    }
+
+    private void parseUpdatedToExisted(Book updatedBook, Book existingBook) {
+        existingBook.setAuthor(updatedBook.getAuthor());
+        existingBook.setName(updatedBook.getName());
+        existingBook.setIsbn(updatedBook.getIsbn());
+        existingBook.setDescription(updatedBook.getDescription());
+        existingBook.setGenre(updatedBook.getGenre());
+    }
+
+    @Override
+    public void deleteBookById(Long id) {
+        Book book = repository.findById(id).orElseThrow(
+                () -> new NoSuchElementException("Book with id %s not found".formatted(id))
+        );
+        repository.delete(book);
+    }
 }
