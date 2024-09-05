f5fab7fefff0933ea7ab37476d887aa59de995ec
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-09-05 16:47:07 +0300

    Add security validation in gateway-service
    - Added dependencies connected with jwt tokens
    - Created gateway filter to process authorization
    - provided JwtUtil class to validate jwt

---
+++ gateway-service/src/main/java/com/artsem/api/gatewayservice/config/GatewayConfig.java
@@ -0,0 +1,30 @@
+package com.artsem.api.gatewayservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class GatewayConfig {
+
+    private final AuthenticationFilter authenticationFilter;
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth-service", r -> r.path("/api/v1/auth/**")
+                        .filters(f -> f.filter(authenticationFilter))
+                        .uri("lb://auth-service"))
+                .route("core-service", r -> r.path("/api/**")
+                        .filters(f -> f.filter(authenticationFilter))
+                        .uri("lb://core-service"))
+                .route("library-service", r -> r.path("/api/**")
+                        .filters(f -> f.filter(authenticationFilter))
+                        .uri("lb://library-service"))
+                .build();
+    }
+}
