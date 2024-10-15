7e15d296823fe22aa2e9e28baad47e8aa12c206f
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-10-15 19:20:06 +0300

    initial commit

--- crud-service/src/main/java/com/artsem/api/crudservice/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.artsem.api.crudservice.repository;
-
-import com.artsem.api.crudservice.model.UserDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface UserRepository extends JpaRepository<UserDetails, Long>, JpaSpecificationExecutor<UserDetails> {
-}
