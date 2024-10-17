ee27b2982051966e8ba4911948faa06b2aea5cbe
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-10-17 22:24:38 +0300

    Remove unused exception - DataNotFoundedException.java

--- keycloak-auth-service/src/main/java/com/artsem/api/keycloakauthservice/exception/DataNotFoundedException.java
+++
@@ -1,7 +0,0 @@
-package com.artsem.api.keycloakauthservice.exception;
-
-public class DataNotFoundedException extends RuntimeException {
-    public DataNotFoundedException(String message) {
-        super(message);
-    }
-}
