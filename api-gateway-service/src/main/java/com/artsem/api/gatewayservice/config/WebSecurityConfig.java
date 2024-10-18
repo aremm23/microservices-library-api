2b8594d3ae5307bc1623ec582b9d6cbf9f072122
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-10-18 21:07:19 +0300

    fix(gateway-service): minor changes

--- api-gateway-service/src/main/java/com/artsem/api/gatewayservice/config/WebSecurityConfig.java
+++ api-gateway-service/src/main/java/com/artsem/api/gatewayservice/config/WebSecurityConfig.java
@@ -6,16 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
 @Configuration
 public class WebSecurityConfig {
-
-    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-    private String jwkSetUri;
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
@@ -32,10 +27,5 @@ public class WebSecurityConfig {
                 .jwt(Customizer.withDefaults()));
         http.csrf(ServerHttpSecurity.CsrfSpec::disable);
         return http.build();
-    }
-
-    @Bean
-    public ReactiveJwtDecoder jwtDecoder() {
-        return NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri).build();
     }
 }
