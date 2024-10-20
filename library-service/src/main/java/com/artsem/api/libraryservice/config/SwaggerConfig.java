3f074a6b447972b7e0c24cda4367a2556b720b4f
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-10-20 11:18:25 +0300

    feature(library-service): Add SwaggerConfig to provide configuration for swagger

---
+++ library-service/src/main/java/com/artsem/api/libraryservice/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.artsem.api.libraryservice.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")))
+                .info(new Info()
+                        .title("Library Service API")
+                        .description("This API manages library functionalities, including handling book statuses, retrieving available books, and updating their engagement statuses. It provides endpoints for taking and returning books.")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Artsem Maiseyenka")
+                                .url("https://github.com/aremm23")
+                        )
+                );
+    }
+}
