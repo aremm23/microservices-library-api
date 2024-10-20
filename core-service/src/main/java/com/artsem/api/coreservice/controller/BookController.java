30815d8a0336e4c86fb1286cf28597c9176a5734
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-10-20 09:52:02 +0300

    feature(core-service): Add description for every endpoint in BookController

--- core-service/src/main/java/com/artsem/api/coreservice/controller/BookController.java
+++ core-service/src/main/java/com/artsem/api/coreservice/controller/BookController.java
@@ -3,6 +3,11 @@ package com.artsem.api.coreservice.controller;
 import com.artsem.api.coreservice.model.Book;
 import com.artsem.api.coreservice.model.dto.BookDto;
 import com.artsem.api.coreservice.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -21,7 +26,12 @@ public class BookController {
 
     private final ModelMapper mapper;
 
-
+    @Operation(summary = "Create a new book", description = "This endpoint allows creating a new book. After the book is created, a message is sent through RabbitMQ to the library-service.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Book created successfully",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = BookDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid input")
+    })
     @PostMapping
     public ResponseEntity<BookDto> createBook(@RequestBody @Valid BookDto bookDto) {
         Book book = bookService.addBook(mapper.map(bookDto, Book.class));
@@ -31,6 +41,13 @@ public class BookController {
         );
     }
 
+    @Operation(summary = "Update a book", description = "This endpoint updates an existing book by its ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Book updated successfully",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = BookDto.class))}),
+            @ApiResponse(responseCode = "404", description = "Book not found"),
+            @ApiResponse(responseCode = "400", description = "Invalid input")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<BookDto> updateBook(
             @PathVariable Long id,
@@ -43,12 +60,23 @@ public class BookController {
         );
     }
 
+    @Operation(summary = "Delete a book", description = "This endpoint deletes a book by its ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Book deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Book not found")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<HttpStatus> deleteBook(@PathVariable Long id) {
         bookService.deleteBookById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @Operation(summary = "Get a book by ID", description = "This endpoint retrieves a book by its ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Book retrieved successfully",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = BookDto.class))}),
+            @ApiResponse(responseCode = "404", description = "Book not found")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<BookDto> getBookById(@PathVariable Long id) {
         return new ResponseEntity<>(
@@ -57,6 +85,12 @@ public class BookController {
         );
     }
 
+    @Operation(summary = "Get a book by ISBN", description = "This endpoint retrieves a book by its ISBN.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Book retrieved successfully",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = BookDto.class))}),
+            @ApiResponse(responseCode = "404", description = "Book not found")
+    })
     @GetMapping("/isbn/{isbn}")
     public ResponseEntity<BookDto> getBookByIsbn(@PathVariable String isbn) {
         return new ResponseEntity<>(
@@ -65,6 +99,11 @@ public class BookController {
         );
     }
 
+    @Operation(summary = "Get all books", description = "This endpoint retrieves a list of all books.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Books retrieved successfully",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = BookDto.class))})
+    })
     @GetMapping
     public ResponseEntity<List<BookDto>> getAllBooks() {
         return new ResponseEntity<>(
