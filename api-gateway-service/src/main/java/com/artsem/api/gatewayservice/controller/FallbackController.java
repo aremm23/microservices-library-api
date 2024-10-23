955dd8be8c18fe7c354e44f1afcef2e4a1f02f7c
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-10-23 13:16:44 +0300

    feature(gateway-service): enhance FallbackController

--- api-gateway-service/src/main/java/com/artsem/api/gatewayservice/controller/FallbackController.java
+++ api-gateway-service/src/main/java/com/artsem/api/gatewayservice/controller/FallbackController.java
@@ -1,5 +1,7 @@
 package com.artsem.api.gatewayservice.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,12 +9,27 @@ import org.springframework.web.bind.annotation.*;
 public class FallbackController {
 
     @GetMapping("/core-service")
-    public String coreServiceFallback() {
-        return "Core service down";
+    public ResponseEntity<String> coreServiceFallback() {
+        return new ResponseEntity<>(
+                "Core service down",
+                HttpStatus.GATEWAY_TIMEOUT
+        );
     }
 
-    @GetMapping("/test")
-    private String test() {
-        return "Hallo Test";
+    @GetMapping("/auth-service")
+    public ResponseEntity<String> authServiceFallback() {
+        return new ResponseEntity<>(
+                "Auth service down",
+                HttpStatus.GATEWAY_TIMEOUT
+        );
     }
+
+    @GetMapping("/library-service")
+    public ResponseEntity<String> libraryServiceFallback() {
+        return new ResponseEntity<>(
+                "Library service down",
+                HttpStatus.GATEWAY_TIMEOUT
+        );
+    }
+
 }
